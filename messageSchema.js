75e4603e87449c1994636e0eda0ecdb8763549c0
dashcol <v96788271@gmail.com>
2024-11-04 17:30:47 +0530

    final

--- messageSchema.js
+++ messageSchema.js
@@ -4,6 +4,10 @@ import mongoose from "mongoose";
 const messageSchema = new mongoose.Schema({
   username: String,
   text: String,
+  profilePicture: {
+    type: String,
+    default: "/default-avatar.png",
+  },
   timestamp: {
     type: Date,
     default: Date.now(),
--- server.js
+++ server.js
@@ -16,18 +16,31 @@ const io = new Server(server, {
     methods: ["POST", "GET"],
   },
 });
+const activeUsers = new Map();
 
 io.on("connection", (socket) => {
   console.log("Connection made");
 
   socket.on("join", async (data) => {
+    activeUsers.set(socket.id, data.user);
     socket.emit("welcome_message", { text: `Welcome,${data.user}!` });
+    socket.broadcast.emit("user_joined", {
+      username: data.user,
+    });
+
+    socket.on("typing", (data) => {
+      socket.broadcast.emit("user_typing", { username: data.user });
+    });
+
+    socket.on("stop_typing", (data) => {
+      socket.broadcast.emit("user_stopped_typing", { username: data.user });
+    });
 
     const startTime = new Date(Date.now() - 24 * 60 * 60 * 1000);
     const endTime = new Date();
 
     messageModel
-      .find({ timeStamp: { $gte: startTime, $lte: endTime } })
+      .find({ timestamp: { $gte: startTime, $lte: endTime } })
       .sort({ timestamp: 1 })
       .then((messages) => {
         socket.emit("load_messages", messages);
@@ -40,6 +53,8 @@ io.on("connection", (socket) => {
       const message = new messageModel({
         username: data.user,
         text: data.message,
+        profilePicture: data.profilePicture,
+        timestamp: new Date(),
       });
       try {
         await message.save();
@@ -48,5 +63,12 @@ io.on("connection", (socket) => {
         console.error(error);
       }
     });
+  });
+  socket.on("disconnect", () => {
+    const username = activeUsers.get(socket.id);
+    if (username) {
+      io.emit("user_left", { username });
+      activeUsers.delete(socket.id);
+    }
   });
 });
